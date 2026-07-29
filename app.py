3c9ecf178ca67b27ebf3cafdcb1ac984a4b5cdac
Durgaprasad <kagithaladurgaprasad@gmail.com>
2026-07-29 18:24:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -252,8 +252,7 @@ def load_lstm():
     _, config = load_tokenizer_and_config()
     full_model = load_model(
         os.path.join(MODELS_DIR, "lstm_model.h5"), 
-        compile=False, 
-        safe_mode=False
+        compile=False
     )
     return _rebuild_inference_models(full_model, "lstm", config["HID_DIM"])
 
@@ -263,8 +262,7 @@ def load_gru():
     _, config = load_tokenizer_and_config()
     full_model = load_model(
         os.path.join(MODELS_DIR, "gru_model.h5"), 
-        compile=False, 
-        safe_mode=False
+        compile=False
     )
     return _rebuild_inference_models(full_model, "gru", config["HID_DIM"])
 
